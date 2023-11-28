b860f91e4b6eccfde54aa8e050fba481c4efc0e9
Aglison Balieiro da Silva <149736696+aglisonb@users.noreply.github.com>
2023-11-28 18:13:59 -0400

    Add files via upload

--- pongpygame/pongpygame/mypongpygame.py
+++ pongpygame/pongpygame/mypongpygame.py
@@ -1,9 +1,10 @@
 import pygame
 import sys
+import time
 
 
 def ball_movement():
-    global ballx, bally, b_score, p_score
+    global ballx, bally, b_score, p_score, still_playing
     ball.x += ballx * balldirect.x
     ball.y += bally * balldirect.y
     if ball.top <= 0 or ball.bottom >= height:
@@ -17,14 +18,20 @@ def ball_movement():
             p_score += 1
             scoring_sound_effect.play()
         ball_reset()
+        if b_score > 2 or p_score > 2:
+            still_playing = False
     if ball.colliderect(player):
         balldirect.x *= -1
         balldirect.y = ((player.centery - ball.y) / (pallet_size / 2)) * -1
         bounce_sound_effect.play()
+        ballx += 0.25
+        bally += 0.25
     if ball.colliderect(bot):
         balldirect.x *= -1
         balldirect.y = ((bot.centery - ball.y) / (pallet_size / 2)) * -1
         bounce_sound_effect.play()
+        ballx += 0.25
+        bally += 0.25
 
 
 def player_animation():
@@ -72,14 +79,16 @@ player = pygame.Rect(width - 20, height / 2 - 100, pallet_width, pallet_size)
 bot = pygame.Rect(10, height / 2 - 100, pallet_width, pallet_size)
 ball = pygame.Rect(width / 2 - 15, height / 2 - 15, ball_size, ball_size)
 
+# ball and paddle speed
 ballx = 0
 bally = 0
 balldirect = pygame.math.Vector2(1, 0)
 p_speed = 0
 b_speed = 10
 a = 10
+still_playing = True
 
-while True:
+while still_playing:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -102,7 +111,7 @@ while True:
     player_animation()
     ball_movement()
 
-    if ball.x < width / 1.5:
+    if ball.x < width / 2:
         if bot.top <= ball.y:
             bot.top += b_speed
         if bot.bottom >= ball.y:
@@ -125,3 +134,14 @@ while True:
     screen.blit(bot_text, (270, 50))
     pygame.display.flip()
     clock.tick(75)
+
+# END GAME SCREEN
+screen.fill((0, 0, 0))
+if p_score > b_score:
+    end_text = f"Victory"
+else:
+    end_text = f"Defeat"
+end_text_formated = game_font.render(end_text, False, (255, 255, 255))
+screen.blit(end_text_formated, (350, 300))
+pygame.display.update()
+time.sleep(4)
